8f2ca5845c5264fede8555e2be6d25079693fce5
Pawlakov <pmatusek@protonmail.com>
2026-08-09 13:45:11 +0200

    removed rump authentication and changed layout a bit

--- TrainDude.Web.Client/Program.cs
+++ TrainDude.Web.Client/Program.cs
@@ -11,7 +11,6 @@ using System.Threading.Tasks;
 using Mediator;
 
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 using TrainDude.Commands.Requests.HostBuilders;
@@ -31,11 +30,6 @@ public static class Program
         builder.Services.AddScoped<ISender, HttpMediator>();
         builder.Services.AddQueryInputValidation();
         builder.Services.AddCommandInputValidation();
-
-        builder.Services.AddOidcAuthentication(options =>
-        {
-            builder.Configuration.Bind("Google", options.ProviderOptions);
-        });
 
         await builder.Build().RunAsync();
     }
